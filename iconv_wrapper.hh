54e352fd9c234357057ed32b173388cfb9757212
Masamichi Hosoda <trueroad@trueroad.jp>
2016-10-19 20:54:36 +0900

    Separate declarations and definitions of member functions

--- iconv_wrapper.hh
+++ iconv_wrapper.hh
@@ -1,5 +1,5 @@
 //
-// One header file iconv wrapper for C++11 2016-10-18.23
+// One header file iconv wrapper for C++11 2016-10-19.20
 // https://github.com/trueroad/iconv_wrapper/
 //
 // Copyright (C) 2016 Masamichi Hosoda. All rights reserved.
@@ -39,68 +39,28 @@ namespace iconv_wrapper
   {
   public:
     // Open
-    void open (const std::string &fromcode, const std::string &tocode)
-    {
-      if (convdesc == invalid_cd)
-        {
-          close ();
-        }
-      convdesc = iconv_open (tocode.c_str (), fromcode.c_str ());
-      if (convdesc == invalid_cd)
-        {
-          throw std::system_error (errno, std::system_category ());
-        }
-    }
+    void open (const std::string &fromcode, const std::string &tocode);
 
     // Close
-    void close () noexcept
-    {
-      if (convdesc != invalid_cd)
-        {
-          iconv_close (convdesc);
-          convdesc = invalid_cd;
-        }
-    }
+    void close () noexcept;
 
     // Convert
-    std::string convert (const std::string &in)
-    {
-      std::string out (in.size (), '\0');
-      convert (in, nullptr, &out);
-      return out;
-    }
+    std::string convert (const std::string &in);
 
     // Convert (with getting the incomplete input / output
     // when an exception occurs)
     std::string &convert (const std::string &in,
                           std::string::size_type *pinpos,
-                          std::string *pout)
-    {
-      size_t inleft {in.size ()};
-      do_iconv (pout, &in.at (0), &inleft, pinpos);
-      return *pout;
-    }
+                          std::string *pout);
 
     // Get initial sequence
-    std::string get_initial_sequence (void)
-    {
-      std::string out (1, '\0');
-      get_initial_sequence (&out);
-      return out;
-    }
+    std::string get_initial_sequence (void);
 
     // Get initial sequence
-    std::string &get_initial_sequence (std::string *pout)
-    {
-      do_iconv (pout, nullptr, nullptr);
-      return *pout;
-    }
+    std::string &get_initial_sequence (std::string *pout);
 
     // Reset conversion state
-    void reset (void) const noexcept
-    {
-      ::iconv (convdesc, nullptr, nullptr, nullptr, nullptr);
-    }
+    void reset (void) const noexcept;
 
     // Constructor / destructor
     iconv () = default;
@@ -122,37 +82,7 @@ namespace iconv_wrapper
   private:
     // Internal function
     void do_iconv (std::string *pout, const char *inbuf, size_t *pinleft,
-                   std::string::size_type *pinpos = nullptr)
-    {
-      if (pout->empty ())
-        {
-          pout->resize (1);
-        }
-      char *inbuf_tmp {const_cast<char*>(inbuf)};
-      char *outbuf {&pout->at (0)};
-      size_t outleft {pout->size ()};
-      size_t s;
-
-      while ((s = ::iconv (convdesc,
-                           &inbuf_tmp, pinleft,
-                           &outbuf, &outleft)) == static_cast<size_t>(-1))
-        {
-          if (errno != E2BIG)
-            {
-              if (pinpos && inbuf)
-                {
-                  *pinpos = (inbuf_tmp - inbuf);
-                }
-              pout->resize (outbuf - &pout->at (0));
-              throw std::system_error (errno, std::system_category ());
-            }
-          std::string::size_type pos = (outbuf - &pout->at (0));
-          pout->resize (pout->size () * 2);
-          outbuf = &pout->at (pos);
-          outleft = pout->size () - pos;
-        }
-      pout->resize (outbuf - &pout->at (0));
-    }
+                   std::string::size_type *pinpos = nullptr);
 
     // Disable copy
     // Since iconv_t cannot duplicate.
@@ -168,6 +98,97 @@ namespace iconv_wrapper
     // Conversion descriptor
     iconv_t convdesc = invalid_cd;
   };
+
+  inline void iconv::open (const std::string &fromcode,
+                           const std::string &tocode)
+  {
+    if (convdesc == invalid_cd)
+      {
+        close ();
+      }
+    convdesc = iconv_open (tocode.c_str (), fromcode.c_str ());
+    if (convdesc == invalid_cd)
+      {
+        throw std::system_error (errno, std::system_category ());
+      }
+  }
+
+  inline void iconv::close () noexcept
+  {
+    if (convdesc != invalid_cd)
+      {
+        iconv_close (convdesc);
+        convdesc = invalid_cd;
+      }
+  }
+
+  inline std::string iconv::convert (const std::string &in)
+  {
+    std::string out (in.size (), '\0');
+    convert (in, nullptr, &out);
+    return out;
+  }
+
+  inline std::string &iconv::convert (const std::string &in,
+                                      std::string::size_type *pinpos,
+                                      std::string *pout)
+  {
+    size_t inleft {in.size ()};
+    do_iconv (pout, &in.at (0), &inleft, pinpos);
+    return *pout;
+  }
+
+  inline std::string iconv::get_initial_sequence (void)
+  {
+    std::string out (1, '\0');
+    get_initial_sequence (&out);
+    return out;
+  }
+
+  inline std::string &iconv::get_initial_sequence (std::string *pout)
+  {
+    do_iconv (pout, nullptr, nullptr);
+    return *pout;
+  }
+
+  inline void iconv::reset (void) const noexcept
+  {
+    ::iconv (convdesc, nullptr, nullptr, nullptr, nullptr);
+  }
+
+  inline void iconv::do_iconv (std::string *pout,
+                               const char *inbuf, size_t *pinleft,
+                               std::string::size_type *pinpos)
+  {
+    if (pout->empty ())
+      {
+        pout->resize (1);
+      }
+    char *inbuf_tmp {const_cast<char*>(inbuf)};
+    char *outbuf {&pout->at (0)};
+    size_t outleft {pout->size ()};
+    size_t s;
+
+    while ((s = ::iconv (convdesc,
+                         &inbuf_tmp, pinleft,
+                         &outbuf, &outleft)) == static_cast<size_t>(-1))
+      {
+        if (errno != E2BIG)
+          {
+            if (pinpos && inbuf)
+              {
+                *pinpos = (inbuf_tmp - inbuf);
+              }
+            pout->resize (outbuf - &pout->at (0));
+            throw std::system_error (errno, std::system_category ());
+          }
+        std::string::size_type pos = (outbuf - &pout->at (0));
+        pout->resize (pout->size () * 2);
+        outbuf = &pout->at (pos);
+        outleft = pout->size () - pos;
+      }
+    pout->resize (outbuf - &pout->at (0));
+  }
 }
 
 #endif  // INCLUDE_GUARD_ICONV_WRAPPER_H
