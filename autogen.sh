ce9d7e230bee6e4cdbe1c5c159916d25af98c92d
Masamichi Hosoda <trueroad@trueroad.jp>
2019-11-17 21:53:55 +0900

    Update autogen.sh
    From Extract PDFmark
    https://github.com/trueroad/extractpdfmark

--- autogen.sh
+++ autogen.sh
@@ -10,7 +10,7 @@ echo
 echo "*** Creating dummy configure.ac"
 {
     cat <<EOF
-AM_GNU_GETTEXT_REQUIRE_VERSION([0.19.8])
+AM_GNU_GETTEXT_REQUIRE_VERSION([0.19.6])
 AM_GNU_GETTEXT([external])
 EOF
 } > configure.ac
@@ -18,7 +18,15 @@ EOF
 echo
 echo "*** Invoking autopoint"
 
-autopoint
+autopoint ||
+    {
+	echo
+	echo "WARNING: autopoint failed."
+	echo "WARNING: It is required for getting \`config.rpath'."
+	echo "WARNING: Please install autopoint 0.19.6 or newer."
+	echo "WARNING: Depending on the platform, this is either"
+	echo "WARNING: a separate package or part of \`gettext-devel'."
+    }
 
 echo
 echo "*** Leaving temporary directory ${AUTOPOINT_TMP_DIR}"
@@ -26,7 +34,22 @@ cd ${CURRENT_DIR}
 
 echo
 echo "*** Copying config.rpath from temporary directory"
-cp ${AUTOPOINT_TMP_DIR}/config.rpath .
+cp ${AUTOPOINT_TMP_DIR}/config.rpath . ||
+    {
+	echo
+	echo "WARNING: Copy failed."
+	if [ -e config.rpath ]; then
+	    echo "WARNING: Using existing \`config.rpath'."
+	else
+	    echo
+	    echo "ERROR: config.rpath does not exist."
+	    echo "ERROR: Please install autopoint 0.19.6 or newer."
+	    echo "ERROR: Depending on the platform, this is either"
+	    echo "ERROR: a separate package or part of \`gettext-devel'."
+	    echo "ERROR: Or, please copy \`config.rpath'."
+	    exit 1
+	fi
+    }
 
 echo
 echo "*** Deleting temporary directory"
